bfcb3871baeddeb4c16c48d3618b0c5670b6f977
David <david.gulkevich@mail.ru>
2021-11-08 00:45:11 +0300

    feat: add Caesar, ROT-8, Atbash cipher functions

--- myCustomStream.js
+++ myCustomStream.js
@@ -88,8 +88,61 @@ class myTransform extends Transform {
 }
 
 
+//Decode - false (0), Encode - true (1)
+let fullalfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let reversealfabetUpper = Array.from(fullalfabet).reverse().join('');
+let reversealfabetLower = reversealfabetUpper.toLowerCase();
+fullalfabet += fullalfabet.toLowerCase();
+let alfSize = fullalfabet.length;
+let retVal = '';
+let isUpper = false;
+//Decode - true, Encode -false
+function C(encode, data, offset){
+    retVal = '';
+    (encode) ? null : offset = - offset;
+    for(let el of data) {
+        let index = fullalfabet.indexOf(el);
+        if(index < 0){
+            retVal+= el;
+        } else{
+            
+            index = (alfSize + index + offset) % alfSize;
+            (el === el.toUpperCase()) ? isUpper = true :
+            isUpper = false;
+            retVal+= isUpper ?
+            fullalfabet[index].toUpperCase() :
+            fullalfabet[index].toLowerCase();
+        }
+    }
+    return retVal;
+    
+}
+function A(data){
+    let retVal = '';
+    for(let el of data){
+        let index = reversealfabetUpper.indexOf(el);
+        if(index < 0){
+            index = reversealfabetLower.indexOf(el);
+            isUpper = false;
+        } else {
+            isUpper = true;
+        }
+        if(index < 0){
+            retVal+= el;
+        }
+        if(index >= 0){
+            retVal+= isUpper ?
+            fullalfabet[index].toUpperCase() :
+            fullalfabet[index].toLowerCase();
+        }
+    }
+    return retVal;
+}
+let qwe = C(true,'This is secret. Message about "_" symbol!',1);
+let C1C1 = C(true,qwe,1);
+let R0 = C(false,C1C1,8);
 
 
-
+console.log(A(R0));
 
 
