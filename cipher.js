92fdd56d1594abbb2eef468ebb80fde9439381bb
David <david.gulkevich@mail.ru>
2021-11-08 02:00:32 +0300

    refactor: put in a separate file(cipher.js) cipher functions

---
+++ cipher.js
@@ -0,0 +1,56 @@
+//Decode - false (0), Encode - true (1)
+let fullalfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let reversealfabetUpper = Array.from(fullalfabet).reverse().join('');
+let reversealfabetLower = reversealfabetUpper.toLowerCase();
+fullalfabet += fullalfabet.toLowerCase();
+let alfSize = fullalfabet.length;
+let retVal = '';
+let isUpper = false;
+
+function C(encode, data, offset) {
+    retVal = '';
+    (encode) ? null: offset = -offset;
+    for (let el of data) {
+        let index = fullalfabet.indexOf(el);
+        if (index < 0) {
+            retVal += el;
+        } else {
+
+            index = (alfSize + index + offset) % alfSize;
+            (el === el.toUpperCase()) ? isUpper = true:
+                isUpper = false;
+            retVal += isUpper ?
+                fullalfabet[index].toUpperCase() :
+                fullalfabet[index].toLowerCase();
+        }
+    }
+    return retVal;
+
+}
+
+function A(data) {
+    let retVal = '';
+    for (let el of data) {
+        let index = reversealfabetUpper.indexOf(el);
+        if (index < 0) {
+            index = reversealfabetLower.indexOf(el);
+            isUpper = false;
+        } else {
+            isUpper = true;
+        }
+        if (index < 0) {
+            retVal += el;
+        }
+        if (index >= 0) {
+            retVal += isUpper ?
+                fullalfabet[index].toUpperCase() :
+                fullalfabet[index].toLowerCase();
+        }
+    }
+    return retVal;
+}
+
+module.exports = {
+    C,
+    A
+};
--- myCustomStream.js
+++ myCustomStream.js
@@ -1,77 +1,78 @@
-const { Readable, Writable, Transform } = require('stream');
+const {
+    Readable,
+    Writable,
+    Transform,
+} = require('stream');
 const fs = require('fs');
-
-
 
 class MyReadStream extends Readable {
     constructor(filename, options = {}) {
-      super(options);
-      this.filename = filename;
-      this.fd = null;
+        super(options);
+        this.filename = filename;
+        this.fd = null;
     }
     _construct(callback) {
-      fs.open(this.filename, 'r', (err, fd) => {
-        if (err) {
-            callback(err);
-        } else {
-          this.fd = fd;
-          callback();
-        }
-      });
+        fs.open(this.filename, 'r', (err, fd) => {
+            if (err) {
+                callback(err);
+            } else {
+                this.fd = fd;
+                callback();
+            }
+        });
     }
     _read(n) {
-      let buf = Buffer.alloc(n,0,'utf8');
-      fs.read(this.fd, buf, 0, n, null, (err, bytesRead) => {
-        if (err) {
-          this.destroy(err);
-        } else {
-            this.push(
-                bytesRead > 0 ? buf.slice(0, bytesRead) : null
-              );
-            
-        }
-      });
+        let buf = Buffer.alloc(n, 0, 'utf8');
+        fs.read(this.fd, buf, 0, n, null, (err, bytesRead) => {
+            if (err) {
+                this.destroy(err);
+            } else {
+                this.push(
+                    bytesRead > 0 ? buf.slice(0, bytesRead) : null
+                );
+
+            }
+        });
     }
     _destroy(err, callback) {
-      if (this.fd) {
-        fs.close(this.fd, (er) => callback(er || err));
-      } else {
-        callback(err);
-      }
+        if (this.fd) {
+            fs.close(this.fd, (er) => callback(er || err));
+        } else {
+            callback(err);
+        }
     }
 }
 
-
-class myWriteStream extends Writable {
+class MyWriteStream extends Writable {
     constructor(filename) {
         super();
         this.filename = filename;
-      }
-      _construct(callback) {
-        fs.open(this.filename, 'a',  (err, fd) => {
-          if (err) {
-            callback(err);
-          } else {
-            this.fd = fd;
-            callback();
-          }
+    }
+    _construct(callback) {
+        fs.open(this.filename, 'a', (err, fd) => {
+            if (err) {
+                callback(err);
+            } else {
+                this.fd = fd;
+                callback();
+            }
         });
-      }
-      _write(chunk, encoding, callback) {
+    }
+    _write(chunk, encoding, callback) {
         fs.write(this.fd, chunk, callback);
-      }
-      _destroy(err, callback) {
+    }
+    _destroy(err, callback) {
         if (this.fd) {
-          fs.close(this.fd, (er) => callback(er || err));
+            fs.close(this.fd, (er) => callback(er || err));
         } else {
-          callback(err);
+            callback(err);
         }
-      }
+    }
 }
-    
+
 class myTransform extends Transform {
 
-    constructor(funcAndArgsObj, options = {}){
+    constructor(funcAndArgsObj, options = {}) {
         options = Object.assign({}, options, {
             decodeStrings: false
         });
@@ -79,70 +80,15 @@ class myTransform extends Transform {
         this.funcAndArgsObj = funcAndArgsObj;
     }
 
-    _transform(chunk, encoding, callback){
-        
-       
+    _transform(chunk, encoding, callback) {
+
+
         this.push(chunk.toUpperCase());
         callback();
     }
 }
 
-
-//Decode - false (0), Encode - true (1)
-let fullalfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let reversealfabetUpper = Array.from(fullalfabet).reverse().join('');
-let reversealfabetLower = reversealfabetUpper.toLowerCase();
-fullalfabet += fullalfabet.toLowerCase();
-let alfSize = fullalfabet.length;
-let retVal = '';
-let isUpper = false;
-//Decode - true, Encode -false
-function C(encode, data, offset){
-    retVal = '';
-    (encode) ? null : offset = - offset;
-    for(let el of data) {
-        let index = fullalfabet.indexOf(el);
-        if(index < 0){
-            retVal+= el;
-        } else{
-            
-            index = (alfSize + index + offset) % alfSize;
-            (el === el.toUpperCase()) ? isUpper = true :
-            isUpper = false;
-            retVal+= isUpper ?
-            fullalfabet[index].toUpperCase() :
-            fullalfabet[index].toLowerCase();
-        }
-    }
-    return retVal;
-    
+module.exports = {
+    MyWriteStream,
+    MyReadStream,
 }
-function A(data){
-    let retVal = '';
-    for(let el of data){
-        let index = reversealfabetUpper.indexOf(el);
-        if(index < 0){
-            index = reversealfabetLower.indexOf(el);
-            isUpper = false;
-        } else {
-            isUpper = true;
-        }
-        if(index < 0){
-            retVal+= el;
-        }
-        if(index >= 0){
-            retVal+= isUpper ?
-            fullalfabet[index].toUpperCase() :
-            fullalfabet[index].toLowerCase();
-        }
-    }
-    return retVal;
-}
-let qwe = C(true,'This is secret. Message about "_" symbol!',1);
-let C1C1 = C(true,qwe,1);
-let R0 = C(false,C1C1,8);
-
-
-console.log(A(R0));
-
-
